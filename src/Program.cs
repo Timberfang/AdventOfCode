7e3ab36161b5ba69e93a465ea1def12fae9c6ace
Lycaon <lycaon37@proton.me>
2023-12-02 09:00:05 -0600

    feat: Complete day 2, part 1

--- src/Program.cs
+++ src/Program.cs
@@ -8,10 +8,11 @@ namespace AdventOfCode.Solutions
 {
     class Program
     {
-        static void Main()
-        {
-            var Output = Day1.Puzzle(File.ReadAllLines(".\\input.txt"));
-            Console.WriteLine($"Output: {Output}");
-        }
+         static void Main()
+         {
+             // var Output = Day1.Puzzle(File.ReadAllLines(".\\input.txt"));
+             var Output = Day2.Puzzle(File.ReadAllLines(".\\input.txt"),12,13,14);
+             Console.WriteLine($"Output: {Output}");
+         }
     }
 }
---
+++ src/solutions/Day2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Solutions
+{
+    public class Day2
+    {
+        public static int Puzzle(string[] Input, int MaxRed, int MaxGreen, int MaxBlue)
+        {
+            if (File.Exists(@".\log.txt")) { File.Delete(@".\log.txt"); }
+
+            int MatchTotal = 0;
+            int NeededCubes = 0;
+            foreach (string game in Input)
+            {
+                Regex MatchNumber = new Regex(@"Game \d*");
+                Regex BlueGames = new Regex(@"\d* blue");
+                Regex RedGames = new Regex(@"\d* red");
+                Regex GreenGames = new Regex(@"\d* green");
+
+                int Match = ExtractNumber(game, MatchNumber).First();
+                int BlueCubes = ExtractNumber(game, BlueGames).Max();
+                int RedCubes = ExtractNumber(game, RedGames).Max();
+                int GreenCubes = ExtractNumber(game, GreenGames).Max();
+                int MatchCubes = BlueCubes * RedCubes * GreenCubes;
+
+                if (BlueCubes <= MaxBlue && RedCubes <= MaxRed && GreenCubes <= MaxGreen)
+                {
+                    File.AppendAllText(@".\log.txt", $"Game {Match} -  Blue Cubes: {BlueCubes}, Red Cubes: {RedCubes}, Green Cubes: {GreenCubes}" + Environment.NewLine);
+                    File.AppendAllText(@".\log.txt", $"Conditions met, adding to sum..." + Environment.NewLine + Environment.NewLine);
+                    MatchTotal += Match;
+                    NeededCubes += MatchCubes;
+                }
+                else { File.AppendAllText(@".\log.txt", $"Conditions NOT met, skipping..." + Environment.NewLine + Environment.NewLine); }
+            }
+
+            return MatchTotal;
+        }
+
+        private static List<int> ExtractNumber(string Input, Regex Filter)
+        {
+            List<int> Count = new List<int>();
+            Regex Numbers = new Regex("[^0-9]+");
+
+            string[] Cubes = Filter.Matches(Input).OfType<Match>().Select(match => match.Value).ToArray();
+            foreach (string Match in Cubes)
+            {
+                 Count.Add(int.Parse(Numbers.Replace(Match, string.Empty)));
+            }
+
+            return Count;
+        }
+    }
+}
